9ed521ce1c53ee180058c396ef766b9dd77791ae
rxgoodness <adedokuntoyyib@gmail.com>
2022-05-19 04:38:24 +0100

    Real-estate-app

--- src/config/connect.js
+++ src/config/connect.js
@@ -22,17 +22,17 @@ import mysql from 'mysql';
 
 
 
-   host:process.env.DB_HOST,
-   user:process.env.DB_USER,
-   password:process.env.DB_PASS,
-   database:process.env.DB_NAME
-        //    host: 'localhost',
-        //    user:'root',
-        //    password:"@Ata287693",
-        //    database: "Apexhauz",
-        //   connectionLimit: 10,
-        //   port:3306,
-        //   database: 'Apexhauz'
+//    host:process.env.DB_HOST ,
+//    user:process.env.DB_USER ,
+//    password:process.env.DB_PASS ,
+//    database:process.env.DB_NAME 
+           host: 'localhost',
+           user:'root',
+           password:"@Ata287693",
+           database: "Apexhauz",
+          connectionLimit: 10,
+          port:3306,
+          database: 'Apexhauz'
 
 
 
